62d5cb5925dc53fb646ea7e4fdeb6a12710700dc
9000Orc <kateyko_hitman@mail.ru>
2018-10-15 19:19:25 +0300

    KNIGHT COMMIT
    Signed-off-by: 9000Orc <kateyko_hitman@mail.ru>

---
+++ core/src/com/bow/game/model/Ally.java
@@ -0,0 +1,107 @@
+package com.bow.game.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.Random;
+
+public abstract class Ally extends GameObject {
+
+    private HealthBar healthBar;
+    private float damage;
+
+    public Ally(TextureRegion texture, TextureAtlas HPtextureAtlas, float x, float y, float width, float height, float maxHealthPoints, float damage) {
+        super(texture, x, y, width, height);
+        this.healthBar = new HealthBar(HPtextureAtlas.findRegion("100"), x, y + height, width, width * 0.07843f, maxHealthPoints);
+        this.damage = damage;
+    }
+
+    /**
+     * Randomly spawn enemy in range of screen width
+     * @param random random function
+     * @param width of the screen
+     * @param speedX horizontal speed
+     * @param speedY vertical speed
+     */
+    public void randomSpawn(Random random, float width, float speedX, float speedY) {
+        float height = width * (float) Gdx.graphics.getHeight() / Gdx.graphics.getWidth();
+
+        float min = -width / 2;
+        float max = width / 2 - getWidth();
+        float delta = max - min;
+        this.setPosition(random.nextFloat() * (delta) + min, height / 2 + (random.nextFloat() * 5f));
+
+        this.healthBar.setPosition(this.getX(), this.getY() - this.healthBar.getHeight());
+        setSpeed(speedX, speedY);
+        healthBar.setSpeed(speedX, speedY);
+    }
+
+    /**
+     * Target spawn at any point on batch
+     * @param x coordinate
+     * @param y coordinate
+     */
+    public void targetSpawn(float x, float y, float speedX, float speedY) {
+        this.setPosition(x, y);
+        this.setSpeed(speedX, speedY);
+        this.healthBar.setSpeed(speedX, speedY);
+    }
+
+    /**
+     * Enemy has been damaged
+     * @param value dealt damaged
+     */
+    public void damaged(float value) {
+        healthBar.damage(value);
+    }
+
+    @Override
+    public void handle() {
+        super.handle();
+        healthBar.handle();
+
+        healthBar.setSpeedX(this.speedX);
+        healthBar.setSpeedY(this.speedY);
+    }
+
+    public void update(TextureAtlas HPtextureAtlas) {
+        if (healthBar.isNeedUpdate()) {
+            healthBar.update(HPtextureAtlas);
+            healthBar.setNeedUpdate(false);
+        }
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+        healthBar.draw(batch);
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x, y);
+        healthBar.setPosition(x, y - this.healthBar.getHeight());
+    }
+
+    public void setDamage(float damage) {
+        this.damage = damage;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public float getHealthPoints() {
+        return healthBar.getHealthPoints();
+    }
+
+    public float getPercentHealthPoints() {
+        return healthBar.getPercentHealthPoints();
+    }
+
+    public float getMaxHealthPoints() {
+        return healthBar.getMaxHealthPoints();
+    }
+}
